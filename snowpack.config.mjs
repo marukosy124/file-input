91f5ce096293da98b1f694658f4bb4d0aca4f46a
viact <soniayeung12435@gmail.com>
2021-07-12 16:56:45 +0800

    initial commit

--- snowpack.config.mjs
+++ snowpack.config.mjs
@@ -1,11 +1,10 @@
 /** @type {import("snowpack").SnowpackUserConfig } */
 export default {
   mount: {
-    /* ... */
+    public: { url: '/', static: true },
+    src: { url: '/dist' },
   },
-  plugins: [
-    /* ... */
-  ],
+  plugins: ['@snowpack/plugin-react-refresh', '@snowpack/plugin-dotenv'],
   routes: [
     /* Enable an SPA Fallback in development: */
     // {"match": "routes", "src": ".*", "dest": "/index.html"},
